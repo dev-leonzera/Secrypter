cc7aecbecabd28088968e1fb6d7364bebab64f90
Leonardo <leonandrade22@gmail.com>
2020-10-20 19:39:31 -0300

    MVP 2
    feat: password length provided by the user

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -1,19 +1,19 @@
 <template>
     <h1>Secrypter</h1>
-    <div class="generator-container">
-      <div class="result-body">
-        <span id="desc">Escolha a quantidade de caracteres para sua senha</span>
-        <input v-model="chars" type="number" id="length" min='4' max='32'/>
+    <form @submit.prevent="gerarSenhaFinal">
+      <div class="generator-container">
+        <div class="result-body">
+          <span id="desc">Escolha o tamanho da sua senha:</span>
+          <input v-model="size" type="number" id="length" min='4' max='32'/>
+          <br/>
+          <button type="submit">Gerar Senha</button>
+        </div>
+        <div class="generator">
+          <input type="text" id="passgen" :value="pass">
+          <button @click="copiarSenha">Copiar</button>
+        </div>
       </div>
-      <div class="generator">
-        <input type="text" id="passgen" :value="pass">
-        <button @click="gerarSenha">Gerar Senha</button>
-        <button class="btn" id="clipboard">
-			    <i class="far fa-clipboard"></i>
-		    </button>
-        <button @click="copiarSenha">Copiar</button>
-      </div>
-    </div>
+    </form>
 </template>
 
 <script>
@@ -26,36 +26,32 @@ export default {
   data() {
     return {
       pass: '',
-      chars: ''
+      size: ''
     }
   },
 
   methods: {
-      gerarSenha: function(){
-          api.listar().then( res => {
-            this.pass = res.data
-            alert('Senha gerada com sucesso!')
-          })
-      },
-      gerarSenhaFinal: function(){
-          api.criar('/generator', this.chars).then( res => {
-            this.pass = res.data
-            alert('Senha gerada com sucesso!')
-          })
-      },
-      copiarSenha () {
-          let senhaCopiada = document.querySelector('#passgen')
-          senhaCopiada.setAttribute('type', 'text')
-          senhaCopiada.select()
-          try {
-            var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-            alert('Testing code was copied ' + msg);
-          } catch (err) {
-            alert('Oops, unable to copy');
-          }
-          window.getSelection().removeAllRanges()
-        },
+    gerarSenhaFinal: function(){
+      console.log(this.size)
+      console.log(typeof(this.size))
+      api.criar(this.size).then( res => {
+        this.pass = res.data
+        alert('Senha gerada com sucesso!')
+      })
+    },
+    copiarSenha () {
+      let senhaCopiada = document.querySelector('#passgen')
+      senhaCopiada.setAttribute('type', 'text')
+      senhaCopiada.select()
+      try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        alert('Testing code was copied ' + msg);
+      } catch (err) {
+        alert('Oops, unable to copy');
+      }
+      window.getSelection().removeAllRanges()
+    },
   }
 }
 </script>
@@ -63,9 +59,15 @@ export default {
   body{
     background-color:#333;
   }
+
   .result-body{
-    border: 1px solid red;
+    padding-bottom: 2rem;
   }
+
+  .result-body span{
+    padding-right: 10px;
+  }
+
   h1, #desc{
     color: white;
   }
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,9 @@ import { http } from './config'
 
 export default {
     listar:() => http.get('/generator'),
-    criar:() => http.post(),
+    criar:(size) => {
+       return http.post('/generator', {size: size})
+    },
     atualizar:() => http.put('/generator'),
     deletar:() => http.delete('/generator')
 }
